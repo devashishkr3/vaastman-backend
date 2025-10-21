19a7df1a72ee6cba617a0b6c62bc270c35819d4d
Devashish Kumar <dk3127197@gmail.com>
2025-10-21 15:33:10 +0530

    app.js updated with admin routes

--- src/routers/app.js
+++ src/routers/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authRoute = require("./authRoute");
+const adminRoute = require("./adminRoute");
 
 router.use("/auth", authRoute);
+router.use("/admin", adminRoute);
 
 module.exports = router;
