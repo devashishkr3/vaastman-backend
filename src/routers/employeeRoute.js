b08803391435ae8a37d61cd14ab687b89e2b6cb2
Devashish Kumar <dk3127197@gmail.com>
2025-10-26 00:12:06 +0530

    employee dashboard

--- src/routers/employeeRoute.js
+++ src/routers/employeeRoute.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const certificateController = require("../controllers/certificateController");
 const { protect, restrictTo } = require("../middlewares/authMIddleware");
 const router = express.Router();
 
 router.use("/", protect, restrictTo("EMPLOYEE"));
 
 // router.get("/dashboard", )
+router.post("/certificate/create", certificateController.createCertificate);
+
+module.exports = router;
