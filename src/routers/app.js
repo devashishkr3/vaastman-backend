b04a08002342c7440b663bf3189d0cea3b3e3bfb
Devashish Kumar <dk3127197@gmail.com>
2025-10-25 13:55:02 +0530

    updated

--- src/routers/app.js
+++ src/routers/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const authRoute = require("./authRoute");
 const adminRoute = require("./adminRoute");
+const employeeRoute = require("./employeeRoute");
 
 router.use("/auth", authRoute);
 router.use("/admin", adminRoute);
+router.use("/employee", employeeRoute);
 
 module.exports = router;
