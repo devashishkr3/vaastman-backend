f61316e8aff1a0763aa6615bf2947c71d6d30c0e
Devashish Kumar <dk3127197@gmail.com>
2025-10-25 21:53:08 +0530

    updated

--- src/utils/buildCertificateHTML.js
+++ src/utils/buildCertificateHTML.js
@@ -1,15 +1,15 @@
+//
 const fs = require("fs");
 const path = require("path");
 
 exports.buildCertificateHTML = (data) => {
-  // const filePath = path.join(
-  //   process.cwd(),
-  //   "src/templates/internship_certificate.html"
-  // );
   const filePath = path.join(
     __dirname,
     "../templates/internship_certificate.html"
   );
+  if (!fs.existsSync(filePath)) {
+    throw new Error("Template file not found at: " + filePath);
+  }
   let html = fs.readFileSync(filePath, "utf8");
 
   Object.keys(data).forEach((key) => {
