ed59c1245c5a8a9a2fa81374ac05280a8fb1ecfe
Devashish Kumar <dk3127197@gmail.com>
2025-10-26 00:12:25 +0530

    global search api

--- src/routers/publicRoute.js
+++ src/routers/publicRoute.js
@@ -4,6 +4,8 @@ const publicController = require("../controllers/publicController");
 const joiValidator = require("../middlewares/joiValidator");
 const contactValidator = require("../validators/contactValidaton");
 const careerValidator = require("../validators/careerValidaton");
+const certificateController = require("../controllers/certificateController");
+const certificateValidator = require("../validators/certificateValidaton");
 
 router.post(
   "/contact",
@@ -14,6 +16,14 @@ router.post(
   "/career",
   joiValidator(careerValidator.createCareerSchema, "body"),
   publicController.submitCareer
+);
+
+router.get("/certificates/search", publicController.globalCertificateSearch);
+
+router.get(
+  "/certificates/verify/:hash",
+  joiValidator(certificateValidator.verifyHashParamSchema, "params"),
+  certificateController.verifyCertificate
 );
 
 module.exports = router;
