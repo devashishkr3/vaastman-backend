f13a661a407358533872fe3ec31c8f570e87eb30
Devashish Kumar <dk3127197@gmail.com>
2025-10-31 14:41:34 +0530

    feat : resolving cors issue for localhost

--- src/index.js
+++ src/index.js
@@ -33,14 +33,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:8080",
+  "http://localhost:3000",
+];
+
 // CORS
 app.use(
   cors({
-    origin:
-      process.env.FRONTEND_URL ||
-      "http://localhost:8080" ||
-      "http://localhost:3000" ||
-      "*",
+    // origin:
+    //   process.env.FRONTEND_URL ||
+    //   "http://localhost:8080" ||
+    //   "http://localhost:3000" ||
+    //   "*",
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
